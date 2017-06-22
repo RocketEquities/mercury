1c057a5ddecc175c9d3fb375e47a7dcf932218f5
Leslie <resurii14@gmail.com>
2017-06-22 23:10:14 +0800

    Fixed form type

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
 
     var register = $.ajax({
       url: "http://ec2-54-255-250-104.ap-southeast-1.compute.amazonaws.com:8000/v1/signups",
-      method: "POST",
+      type: "POST",
       contentType: "application/json",
       data: $(this).serialize()
     });
